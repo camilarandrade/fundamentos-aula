61d80ef67b195aa801dce8c2a1f41fc47a06a383
Camila Andrade <camila.rda@outlook.com>
2024-08-06 13:08:51 -0300

    Feat: Add js events and functions

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+let imagem = document.querySelector('.imagem')
+let video= document.querySelector ('#video')
+
+function ImagemZoom() {
+    imagem.style.width = '700px'
+    imagem.style.height = '700px'
+
+}
+
+function ImagemNormal() {
+    imagem.style.width = '500px'
+    imagem.style.height = '500px'
+
+}
+
+function VideoZoom() {
+    video.style.width = '660px'
+    video.style.height = '415px'
+
+}
+
+function VideoNormal() {
+    video.style.width = '560px'
+    video.style.height = '315px'
+
+}
